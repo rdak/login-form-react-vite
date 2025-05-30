564505de11ed49f0c46eb850689e10fda1e6d541
rdak <seva@chebykin.com>
2025-05-31 00:08:58 +0200

    add oklch

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import postcssPresetEnv from "postcss-preset-env";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+    css: {
+        postcss: {
+            plugins: [postcssPresetEnv],
+        },
+    },
+});
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,22 +1,11 @@
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 
-// export default defineConfig({
-// 	plugins: [react()],
-// 	test: {
-// 		browser: {
-// 			enabled: true,
-// 			provider: "playwright",
-// 			instances: [{ browser: "chromium" }],
-// 		},
-// 	},
-// });
-
 export default defineConfig({
-	plugins: [react()],
-	test: {
-		environment: "jsdom",
-		globals: true,
-		setupFiles: "./src/setupTests.ts",
-	},
+    plugins: [react()],
+    test: {
+        environment: "jsdom",
+        globals: true,
+        setupFiles: "./src/setupTests.ts",
+    },
 });
