4f7c67dc0ee16f69fadb5a141b19173b42b15e4c
rdak <seva@chebykin.com>
2025-05-31 19:43:40 +0200

    disallow any colors but oklch

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,4 +1,12 @@
 export default {
     extends: ["stylelint-config-standard-scss"],
+    plugins: ["stylelint-gamut"],
     ignoreFiles: ["**/*.css"],
+    rules: {
+        "gamut/color-no-out-gamut-range": true,
+        "function-disallowed-list": ["rgba", "hsla", "rgb", "hsl"],
+        "color-function-notation": "modern",
+        "color-no-hex": true,
+        "color-named": "never",
+    },
 };
