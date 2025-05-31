561a65e06a62e02f316e188540343fef6d736991
rdak <seva@chebykin.com>
2025-06-01 00:03:09 +0200

    add svg

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import postcssPresetEnv from "postcss-preset-env";
+import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
-    plugins: [react()],
+    plugins: [svgr(), react()],
     css: {
         postcss: {
             plugins: [postcssPresetEnv],
